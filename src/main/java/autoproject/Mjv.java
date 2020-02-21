c3b6bfc7a43383c57ffdd0847de86fe280789226
Shobhan <shobhan.ks@testyantra.com>
2020-02-21 16:31:50 +0530

    asf

--- src/main/java/autoproject/Mjv.java
+++ src/main/java/autoproject/Mjv.java
@@ -3,7 +3,9 @@ package autoproject;
 public class Mjv {
 
 	public static void main22(String[] args) {
+		System.out.println("sdb");
 	}
 	public static void main(String[] args) {
+		System.out.println("sdbasdj");
 	}
 }
