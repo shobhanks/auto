6f2644bd681331c6d697a10a1246b8172ab30f63
Shobhan <shobhan.ks@testyantra.com>
2020-02-21 16:29:23 +0530

    jhdg

--- src/main/java/autoproject/Mjv.java
+++ src/main/java/autoproject/Mjv.java
@@ -3,7 +3,9 @@ package autoproject;
 public class Mjv {
 
 	public static void main3(String[] args) {
+		System.out.println("hi");
 	}
 	public static void main4(String[] args) {
+		System.out.println("helllo");
 	}
 }
